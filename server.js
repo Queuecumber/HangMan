0629d4ca214249dfb140fd3d60acee8f5d20d867
Max Ehrlich <max.ehr@gmail.com>
2015-01-28 20:49:22 -0500

    Stub server

--- server.js
+++ server.js
@@ -3,4 +3,10 @@ var ltx = require('node-xmpp-core').ltx;
 
 var r = new xmpp.Router();
 
-r.register('hm.queuecumber.net');
+r.register('hm.queuecumber.net', function (stanza)
+{
+    var me = stanza.attrs.to;
+    stanza.attrs.to = stanza.attrs.from;
+    stamza.attrs.from = me;
+    r.send(stanza);
+});
